4f16f73b61c274693dec9d45575ebf7a49546181
Chevillat <jerome@smoothplanet.com>
2019-03-13 21:10:52 +0100

    Unknown modification

--- MyRecursiveAppInCSharp/Form1.Designer.cs
+++ MyRecursiveAppInCSharp/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(152, 172);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(133, 67);
+            this.btn_send.TabIndex = 0;
+            this.btn_send.Text = "Calculer";
+            this.btn_send.UseVisualStyleBackColor = true;
+            // 
+            // txt_result
+            // 
+            this.txt_result.Location = new System.Drawing.Point(152, 315);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(100, 20);
+            this.txt_result.TabIndex = 1;
+            this.txt_result.Text = "cd";
+            this.txt_result.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(458, 450);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.btn_send);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txt_result;
     }
 }
 
